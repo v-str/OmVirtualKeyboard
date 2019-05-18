c3496d8f7d743494b67c1889ae900af4a7014e3d
Ordinary Mind <1.ordinarymind@gmail.com>
2019-05-19 00:53:59 +0300

    Add cmd parser for keyboard

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,20 +1,35 @@
 #include <QApplication>
 #include <QCoreApplication>
+
 #include <QCommandLineParser>
+#include <QCommandLineOption>
 
 #include <omvirtualkeyboard.h>
 
-#define APP_NAME    "omvkbd"
+#define APP_NAME        "omvkbd"
+#define APP_VERSION     "0.10"
+#define APP_DESCRIPTION "omvkbd - virtual keyboard"
 
 void setAppSetting(QApplication *application);
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     QApplication application(argc, argv);
     setAppSetting(&application);
 
+    QCommandLineParser cmdParser;
+    cmdParser.setApplicationDescription(APP_DESCRIPTION);
+    cmdParser.addHelpOption();
+    cmdParser.addVersionOption();
+
+    QCommandLineOption position({ "p", "position" },
+                                "Set position relative desktop screen",
+                                "",
+                                "100x100");
+    cmdParser.addOption(position);
 
 
+    cmdParser.process(application);
 
     OmVirtualKeyboard vkdb;
     vkdb.showKeyboard();
@@ -24,4 +39,5 @@ int main(int argc, char *argv[])
 
 void setAppSetting(QApplication * application){
     application->setApplicationName(APP_NAME);
+    application->setApplicationVersion(APP_VERSION);
 }
--- sources/omvirtualkeyboard.cpp
+++ sources/omvirtualkeyboard.cpp
@@ -5,10 +5,12 @@
 #include <QSizePolicy>
 #include <QApplication>
 #include <QResizeEvent>
+#include <QDesktopWidget>
 
 #define APP_NAME    "omvkbd"
 #define WIDTH       650
 #define HEIGHT      255
+#define MAGIC       100
 
 OmVirtualKeyboard::OmVirtualKeyboard(QWidget *parent) :
     QFrame(parent),
@@ -25,19 +27,29 @@ OmVirtualKeyboard::~OmVirtualKeyboard()
 
 void OmVirtualKeyboard::showKeyboard()
 {
+    QRect desktopGeometry = QApplication::desktop()->geometry();
+    short desktopWidth = desktopGeometry.width();
+    short desktopHeigh = desktopGeometry.height();
+
+    short x = (desktopWidth / 2) - (geometry().width() / 2);
+    short y = desktopHeigh - geometry().height() - MAGIC;
+
+    move(x,y);
+
     show();
 }
 
 void OmVirtualKeyboard::resizeEvent(QResizeEvent *event)
 {
     event->ignore();
+    QFrame::resizeEvent(event);
 }
 
 void OmVirtualKeyboard::setInitialSetting()
 {
     ui->setupUi(this);
     setWindowTitle(APP_NAME);
-    setWindowFlags(Qt::WindowDoesNotAcceptFocus |
+    setWindowFlags(Qt::WindowDoesNotAcceptFocus | Qt::Tool |
                    Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
     setFixedSize(650,255);
 }
