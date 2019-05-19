9a35be52e2870b6ea939800e33d05484b6f2cc90
Ordinary Mind <1.ordinarymind@gmail.com>
2019-05-19 23:32:49 +0300

    Add initial grid layout

--- headers/keyboardgridlayoutconstructor.h
+++ headers/keyboardgridlayoutconstructor.h
@@ -2,19 +2,34 @@
 #define KEYBOARD_GRID_LAYOUT_CONSTRUCTOR_H
 
 #include <QObject>
+#include <QVector>
+#include <QChar>
+#include <QPair>
 
 class QWidget;
 class QGridLayout;
 
 class KeyboardGridLayoutConstructor : public QObject
 {
+    Q_OBJECT
 public:
     explicit KeyboardGridLayoutConstructor(QObject * parent = Q_NULLPTR);
     ~KeyboardGridLayoutConstructor() = default;
 
     void prepareGridLayout(QWidget * mainWidget);
+    QGridLayout * gridLayout();
+
+signals:
+    void keyClicked(const QString & text);
+
+private slots:
+    void keyButtonClicked(const QString & text);
 
 private:
+    void constructDefaultKeysArray();
+
+    QVector<QPair<Qt::Key, QChar> > m_keySet;
+
     QGridLayout * m_pGridLayout = Q_NULLPTR;
 };
 
